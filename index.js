c36834c0a4acaa75aa7fdf79745a45b2a515667a
Austen Brown <BrownsterJr@gmail.com>
2018-03-23 16:39:05 +0000

    nicer command check
    made it so not every message gets fully checked

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const config = require('./config.json');
 client.login(config.token);
 
 client.on('message', function(msg){
-  if(msg.content === config.prefix + 'ping') {
-    msg.reply('pong');
+  if(msg.content[0] != config.prefix){return}
+  var reqCmd = msg.content.slice(1);
+  if(reqCmd.toString() === 'ping'){
+    msg.channel.send('pong');
   }
 });
